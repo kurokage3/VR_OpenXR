9189649ef27012b004d1a460a67c8eecf07aaba8
kurokage3 <kurokage130@gmail.com>
2024-02-19 21:16:43 -0800

    Solved XRGrabInteractableBothHands  script
    Using GameObject Tags for the "Right Hand" & "Left Hand" I was able to get a solve any issues with the detection of which controller is grabbing the interactable object.
    
    Also, I set up some targets and it all works!

--- Assets/Scripts/XRGrabInteractableBothHands.cs
+++ Assets/Scripts/XRGrabInteractableBothHands.cs
@@ -4,6 +4,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRGrabInteractableBothHands : XRGrabInteractable
@@ -12,39 +13,70 @@ public class XRGrabInteractableBothHands : XRGrabInteractable
     public Transform leftAttachTransform;
     public Transform rightAttachTransform;
 
-    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    protected override void OnSelectEntering(SelectEnterEventArgs args)
     {
-        //Get the XRController from the interactorObject
-        XRBaseInteractor interactor = args.interactorObject as XRBaseInteractor;
-        if (interactor == null)
-        {
-            Debug.LogWarning("InteractorObject is not of type XRBaseInteractor");
-            return;
-        }
-        XRController controller = interactor.GetComponent<XRController>();
+        string name = args.interactorObject.transform.name;
+        Debug.Log("interactorObject Name = " + name);
 
-        //If the controller is not found directly, attempt to find it in the parent
-        if (controller == null)
+        if (args.interactorObject.transform.CompareTag("Left Hand"))
         {
-            controller = interactor.GetComponentInParent<XRController>();
+            attachTransform = leftAttachTransform;
+        }
+        else if(args.interactorObject.transform.CompareTag("Right Hand"))
+        {
+            attachTransform = rightAttachTransform;
         }
 
-        // heck if the controller component is present
-        if (controller != null)
-        {
-            //If Left Controller, set the left attach transform
-            if (controller.controllerNode == UnityEngine.XR.XRNode.LeftHand && leftAttachTransform != null)
-            {
-                attachTransform = leftAttachTransform;
-            }
-            //Else If Right Controller, set the right attach transform
-            else if (controller.controllerNode == UnityEngine.XR.XRNode.RightHand && rightAttachTransform != null)
-            {
-                attachTransform = rightAttachTransform;
-            }
-        }
-
-        //Keep all normal XRGrabInteractable functionality
-        base.OnSelectEntered(args);
+        base.OnSelectEntering(args);
     }
+
+    //protected override void OnSelectEntered(SelectEnterEventArgs args)
+    //{
+    //    base.OnSelectEntered(args);
+
+    //    // Try to get the ActionBasedController from the interactor
+    //    var controller = args.interactorObject as ActionBasedController;
+    //    if (controller == null)
+    //    {
+    //        Debug.LogWarning("The interactor does not have an ActionBasedController component.", this);
+    //        return;
+    //    }
+
+    //    // Determine the node (left or right hand) associated with the controller
+    //    var controllerNode = GetControllerNode(controller);
+
+    //    // Set the attach transform based on the node
+    //    if (controllerNode == XRNode.LeftHand && leftAttachTransform != null)
+    //    {
+    //        attachTransform = leftAttachTransform;
+    //    }
+    //    else if (controllerNode == XRNode.RightHand && rightAttachTransform != null)
+    //    {
+    //        attachTransform = rightAttachTransform;
+    //    }
+    //}
+
+    //private XRNode GetControllerNode(ActionBasedController controller)
+    //{
+    //    var inputDevices = new List<InputDevice>();
+    //    InputDevices.GetDevices(inputDevices);
+
+    //    foreach (var device in inputDevices)
+    //    {
+    //        if (device.characteristics.HasFlag(InputDeviceCharacteristics.HeldInHand))
+    //        {
+    //            // Determine if the device is a left-hand or right-hand controller
+    //            if (device.characteristics.HasFlag(InputDeviceCharacteristics.Left))
+    //            {
+    //                return XRNode.LeftHand;
+    //            }
+    //            else if (device.characteristics.HasFlag(InputDeviceCharacteristics.Right))
+    //            {
+    //                return XRNode.RightHand;
+    //            }
+    //        }
+    //    }
+
+    //    return XRNode.Head; // Default to head if we cannot find the controller node
+    //}
 }
